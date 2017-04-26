0a4ce6d7c9e43f6f5cc7d5f35291caa8d2ca5a4e
Asier Ruano <asierruano@gmail.com>
2017-04-26 21:04:58 +0200

    Add files via upload

---
+++ PracticaFallaDescripcion/flickrClient.js
@@ -0,0 +1,334 @@
+// Funcion que recoge los valores de la búsqueda (Se activa en el botón "Filtrar" del menu)
+
+$(function(){
+  getPhotos();
+});
+
+function addLoadEvent(func) {
+  var oldOnload = window.onload;
+  if (typeof window.onload != 'function') {
+    window.onload = func;
+  } else {
+    window.onload = function() {
+      oldOnload();
+      func();
+    }
+  }
+}
+
+var filtros = {
+  tags: undefined,
+  text: undefined,
+  min_taken_date: undefined,
+  max_taken_date: undefined,
+  min_upload_date: undefined,
+  max_upload_date: undefined,
+  bbox: undefined,
+  extras: undefined,
+  format: "json"
+};
+
+var numImagenes = 0;
+var minViews = undefined;
+
+function getImages() {
+  // Query type indica el tipo de busqueda (Fecha de captura, tamaño, titulo ...) para poder acceder al valor de su formulario
+  var input = query_type + " :input";
+  var value = $(input).val();
+  // En funcion del tipo de consulta, rellenaremos o no ciertos campos
+  switch (query_type) {
+
+    // BUSQUEDA POR FECHA DE CAPTURA
+    case '#fecha-captura-form':
+      var inputs = $(query_type).find("input");
+      filtros.min_taken_date = inputs[0].value;
+      filtros.max_taken_date = inputs[1].value;
+      console.log("min-taken-date " + filtros.min_taken_date);
+      console.log("max-taken-date " + filtros.max_taken_date);
+      break;
+
+    // BUSQUEDA POR LOCALIZACION
+    case '#localizacion-form':
+      // En el caso del tamaño, tendremos que recoger 2 parámetros
+      var longSW = Math.max(rectangle.getBounds().getSouthWest().lng(), -180);
+      var latSW = Math.max(rectangle.getBounds().getSouthWest().lat(), -90);
+      var longNE = Math.min(rectangle.getBounds().getNorthEast().lng(), 180);
+      var latNE = Math.min(rectangle.getBounds().getNorthEast().lat(), 90);
+      filtros.bbox = longSW + "," +  latSW + "," + longNE + "," + latNE;
+      break;
+
+    // BUSQUEDA POR TITULO
+    case '#titulo-form':
+      filtros.text = value;
+      console.log("Titulo " + filtros.text);
+      break;
+
+    // BUSQUEDA POR ETIQUETAS
+    case '#etiquetas-form':
+      filtros.tags = value;
+      console.log("Etiquetas " + filtros.tags);
+      break;
+
+    // BUSQUEDA POR NUMERO DE VISITAS
+    case '#views-form':
+      filtros.extras = "views";
+      minViews = value;
+      console.log("Visitas " + value);
+      break;
+
+    // BUSQUEDA POR FECHA DE SUBIDA
+    case '#fecha-subida-form':
+      var inputs = $(query_type).find("input");
+      filtros.min_upload_date = inputs[0].value;
+      filtros.max_upload_date = inputs[1].value;
+      console.log("min-upload-date " + filtros.min_upload_date);
+      console.log("max-upload-date " + filtros.max_upload_date);
+      break;
+
+    default:
+      console.log("Búsqueda no valida");
+  }
+
+  // Borramos el resultado anterior
+  $(".grid__item").remove();
+  $("img.imagen-flickr").remove();
+  $("img.original").remove();
+
+  // Cargamos nuestros filtros (los no usados toman el valor undefined)
+
+  // Cargamos los filtros en la petición
+  getPhotos();
+  checkFilters();
+}
+
+function getImagesAndHideForm (){
+  $(".form-control-wrapper.menu-form").hide(300);
+  $("#map").hide(300);
+  $("#boton-enviar").hide(300);
+  getImages();
+  $("input").each(function() {resetForm($(this));});
+}
+
+// Funcion que carga las fotos
+
+function getPhotos() {
+  console.log(filtros);
+  $('#numResultadosTagText').css('display', 'none');
+  numImagenes = 0;
+
+  url = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=" + api_key + "&per_page=25&format=json&nojsoncallback=1";
+
+  $.getJSON(url, filtros,
+    function(data) {
+      console.log(data);
+      console.log(url);
+      $.each(data.photos.photo, function(i, photo) {
+
+        var msg = photo.title.substr(0, 1).toUpperCase() + photo.title.substr(1);
+        var valor;  
+        var getdesc = "https://api.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=" + api_key +"&photo_id="+ photo.id +"&format=json&jsoncallback=1";
+          $.getJSON(getdesc, function(des) {
+              console.log(des);
+              console.log(getdesc);
+              valor = photo.des.description._content;
+              
+              //console.log(valor);                     
+          });
+        var album, grupo;  
+        getalbum =  "https://api.flickr.com/services/rest/?method=flickr.photos.getAllContexts&api_key=" + api_key +"&photo_id="+ photo.id +"&format=json&jsoncallback=1";
+          $.getJSON(getalbum, function(album) {
+              console.log(album);
+              console.log(getalbum);
+              album = photo.set;
+              grupo = photo.pool;
+                                
+          });
+          
+
+        var url_img = "https://farm" + photo.farm + ".staticflickr.com/" + photo.server + "/" + photo.id + "_" + photo.secret + "_h.jpg";
+
+        var size = "800 x 800";
+
+        var html = getHtml(url_img, size, msg, valor,album,grupo);
+
+        if(!filtros.extras || parseInt(photo.views) >= minViews){
+          numImagenes++;
+          $(".grid").append(html);
+        }
+      });
+      // Una vez cargadas todas las fotos, se añaden animaciones
+      // Funcion del archivo library/js/animations.js
+      setAnimation();
+      $('#numResultadosTagText').text(numImagenes);
+      $('#numResultadosTagText').css('display', 'inline-block');
+    });
+}
+
+// Funcion para obtener el HTML que se inyectará
+
+function getHtml(url_img, size, msg, valor,album,grupo) {
+  var html = '<div class="grid__item" data-size="';
+  html += size;
+  html += '">';
+  html += '<a href="';
+  html += url_img;
+  html += '" class="img-wrap image"><img class="imagen-flickr" src="';
+  html += url_img;
+  html += '" alt="img04" />';
+  html += '<div class="description description--grid">';
+  html += "Título = "+msg+"  Descripción = ";    
+  html += valor +"  Album = "+ album +"  Grupo = " +grupo;
+  html += '</div>';  
+  html += '</a>';
+  html += '</div>';
+  return html;
+}
+
+
+//Funcion que muestra o esconde las etiquetas de las busquedas
+function checkFilters() {
+
+  var someFilter = false;
+
+  if (filtros.min_taken_date === undefined && filtros.max_taken_date === undefined){
+    $('#fechaCapturaTag').removeClass("display-inline-tag");
+  }
+  else {
+    var s = getFormattedtDate("taken");
+    $("#fechaCapturaTagText").text(s[0]  + " | " + s[1]);
+    $('#fechaCapturaTag').addClass("display-inline-tag");
+    someFilter = true;
+  }
+
+  if (filtros.text === undefined){
+    $('#tituloTag').removeClass("display-inline-tag");
+  }
+  else{
+    $("#tituloTagText").text(filtros.text);
+    $('#tituloTag').addClass("display-inline-tag");
+    someFilter = true;
+  }
+
+  if (filtros.tags === undefined){
+    $('#etiquetasTag').removeClass("display-inline-tag");
+  }
+  else{
+    $("#etiquetasTagText").text(filtros.tags);
+    $('#etiquetasTag').addClass("display-inline-tag");
+    someFilter = true;
+  }
+
+  if (filtros.bbox === undefined){
+    $('#localizacionTag').removeClass("display-inline-tag");
+  }
+  else{
+    var s = getFormattedCoords();
+    $("#localizacionTagText").text(s);
+    $('#localizacionTag').addClass("display-inline-tag");
+    someFilter = true;
+  }
+
+  if (filtros.min_upload_date === undefined && filtros.max_upload_date === undefined){
+    $('#fechaSubidaTag').removeClass("display-inline-tag");
+  }
+  else{
+    var s = getFormattedtDate("upload");
+    $("#fechaSubidaTagText").text(s[0] + " | " + s[1]);
+    $('#fechaSubidaTag').addClass("display-inline-tag");
+    someFilter = true;
+  }
+
+  if (filtros.extras === undefined){
+    $('#visitasTag').removeClass("display-inline-tag");
+  }
+  else{
+    $("#visitasTagText").text("Mínimo "+minViews+" visitas");
+    $('#visitasTag').addClass("display-inline-tag");
+    someFilter = true;
+  }
+
+  if (someFilter){
+    $('#numResultadosTag').show();
+  }
+  else {
+    $('#numResultadosTag').hide();
+  }
+}
+
+//Funcion que elimina del objeto 'filtros' aquellos parametros de busqueda eliminados (al cerrar etieutas)
+function tagToFalse(tag){
+  switch(tag){
+    case 'filtro_fecha_captura':
+      filtros.min_taken_date = undefined;
+      filtros.max_taken_date = undefined;
+      break;
+    case 'filtro_titulo':
+      filtros.text = undefined;
+      break;
+    case 'filtro_etiquetas':
+      filtros.tags = undefined;
+      break;
+    case 'filtro_localizacion':
+      filtros.bbox = undefined;
+      break;
+    case 'filtro_fecha_subida':
+      filtros.min_upload_date = undefined;
+      filtros.max_upload_date = undefined;
+      break;
+    case 'filtro_visitas':
+      filtros.extras = undefined;
+      break;
+  }
+  query_type = "none";
+  getImages();
+}
+
+//Funcion para limpiar los formularios
+function resetForm (form){
+  $(form).val("");
+}
+
+//Formatear fechas
+function getFormattedtDate(t){
+  var s = [];
+  switch(t){
+    case "taken":
+      if (filtros.min_taken_date === ""){
+        s[0] = "Desde siempre";
+      } else{
+        s[0] = filtros.min_taken_date;
+      }
+      if (filtros.max_taken_date === ""){
+        s[1] = "Hasta ahora";
+      } else{
+        s[1] = filtros.max_taken_date;
+      }
+      break;
+    case "upload":
+      if (filtros.min_upload_date === ""){
+        s[0] = "Desde siempre";
+      } else{
+        s[0] = filtros.min_upload_date;
+      }
+      if (filtros.max_upload_date === ""){
+        s[1] = "Hasta ahora";
+      } else{
+        s[1] = filtros.max_upload_date;
+      }
+      break;
+  }
+  return s;
+}
+
+//Formatear coordenadas
+function getFormattedCoords(){
+  var a = filtros.bbox.split(",");
+  var s = "";
+  var i = 1;
+  for (var cAux of a){
+    s += cAux.substring(0,5);
+    if (i < 4) s += ", ";
+    i++;
+  }
+  return s;
+}
